6b994f07faa20ff9f715faf0b00c06184086f070
Takdanai <rookiehome7@gmail.com>
2019-07-23 15:09:29 +0800

    Initial Commit

---
+++ beacon_SearchingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  beacon_SearchingApp
+//
+//  Created by Takdanai Jirawanichkul on 23/7/2562 BE.
+//  Copyright © 2562 Takdanai Jirawanichkul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
